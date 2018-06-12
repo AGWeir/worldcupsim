a2c2860ec6de8d5fb88dcea61f32fdd894014859
AGWeir <AWGWeir@gmail.com>
2018-06-12 20:57:14 +0100

    Update logic.py
    added off,def strength and poisson model

--- logic.py
+++ logic.py
@@ -1,15 +1,19 @@
 from random import randint
+import scipy.stats as ss
 import itertools
 from prettytable import PrettyTable
-import operator
+from numpy.random import choice
+import csv
 
 class team:
     def __init__(self, country):
-        self.name = country
+        self.name = country[0]
         self.gp = 0
         self.ptd = 0
         self.gf = 0
         self.ga = 0
+        self.attstrength = country[1]
+        self.defstrength = country[2]
         
     def __repr__(self):
         return str(self.name)
@@ -20,24 +24,6 @@ class team:
         self.ptd = self.ptd + np
         self.gf = self.gf + gf
         self.ga = self.ga + ga
-        
-def createteams(teams):
-    #accepts list of teams as strings
-    return [team(x) for x in teams]
-
-def playmatch(team1,team2):
-    t1g = randint(0,5)
-    t2g = randint(0,5)
-    print('Result:',team1,t1g,"-",t2g,team2)
-    if t1g > t2g:
-        team1.update(3,t1g,t2g)
-        team2.update(0,t2g,t1g)
-    elif t2g  > t1g:
-        team2.update(3,t2g,t1g)
-        team1.update(0,t1g,t2g)
-    else:
-        team1.update(1,t1g,t2g)
-        team2.update(1,t2g,t1g)
     
 def playallmatches(group):
     pool = tuple(itertools.combinations(group,2))
@@ -52,6 +38,35 @@ def playallmatches(group):
     t.reversesort = True
     print(t)
     
-groupa = [team(x) for x in ['England','Wales','Scotland','Northern Ireland']]
+def playmatch(team1,team2):
+    t1xpg = team1.attstrength * team2.defstrength * 1.18
+    t2xpg = team2.attstrength * team1.defstrength * 1.18
+    
+    t1probs = []
+    t2probs = []
+    scores = []
+    for i in range(0,25):
+        t1probs.append(ss.poisson.pmf(i,t1xpg))
+        t2probs.append(ss.poisson.pmf(i,t2xpg))
+
+    t1g = choice(range(25), 1, p=t1probs)[0]
+    t2g = choice(range(25), 1, p=t2probs)[0]
+    result = str(t1g) + "-" + str(t2g)
+    print('Result:',team1,result,team2)
+    
+    if t1g > t2g:
+        team1.update(3,t1g,t2g)
+        team2.update(0,t2g,t1g)
+    elif t2g  > t1g:
+        team2.update(3,t2g,t1g)
+        team1.update(0,t1g,t2g)
+    else:
+        team1.update(1,t1g,t2g)
+        team2.update(1,t2g,t1g)
+        
+groupa = [team(x) for x in[['Belgium',1.92,0.41],
+                    ['England',1.17,0.55],
+                    ['Tunisia',1.22,0.81],
+                    ['Panama',0.55,1.17]]]
 
 playallmatches(groupa)
