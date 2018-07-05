a3cda64556f1091d804828d65daedef664cf046d
AGWeir <AWGWeir@gmail.com>
2018-07-05 15:50:18 +0100

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,9 +1,8 @@
-from random import randint
-import scipy.stats as ss
-import itertools
-from prettytable import PrettyTable
-from numpy.random import choice
-import csv
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+alldata = pd.read_csv('results.csv')
 
 class team:
     def __init__(self, country):
@@ -24,49 +23,61 @@ class team:
         self.ptd = self.ptd + np
         self.gf = self.gf + gf
         self.ga = self.ga + ga
-    
-def playallmatches(group):
-    pool = tuple(itertools.combinations(group,2))
-    for x in pool:
-        playmatch(x[0],x[1])
-        
-    t = PrettyTable(['Team','P','GF','GA','Pts'])
-    for x in group:
-        t.add_row([str(x),x.gp,x.gf,x.ga,x.ptd])
-    
-    t.sortby = 'Pts'
-    t.reversesort = True
+
+def createteam(teamname):
+    atts,defs = getstrengths(teamname)
+    newteam = team([teamname,atts,defs])
+    return newteam
+	
+def createteams(teams):
+    allteams = []
+    for i in teams:
+        allteams.append(createteam(i))
+    return allteams
+
+def calcstrength(results, team):
+    homes = results[results.home_team == team]
+    aways = results[results.away_team == team]
+    gf = homes.home_score.sum() + aways.away_score.sum()
+    ga = homes.away_score.sum() + aways.home_score.sum()
+    frames = [homes,aways]
+    allgames = pd.concat(frames)
+    gp = len(allgames)
+    return (gf / gp), (ga / gp)
+
+def getstrengths(team):
+	atts, defs = calcstrength(alldata[(alldata['date'].dt.year >= 2017) & (alldata['tournament'] != 'Friendly')],team)
+	return atts,defs
+
+
+## Simulate group stages	
+groupa = ['Uruguay','Russia','Egypt','Saudi Arabia']
+groupb = ['Spain', 'Portugal','Iran','Morocco']
+groupc = ['France','Denmark','Peru','Australia']
+groupd = ['Croatia','Argentina','Nigeria','Iceland']
+groupe = ['Brazil','Switzerland','Serbia','Costa Rica']
+groupf = ['Sweden','Mexico','Korea Republic','Germany']
+groupg = ['England','Belgium','Tunisia','Panama']
+grouph = ['Colombia','Japan','Senegal','Poland']
+
+allgroups = [groupa,groupb,groupc,groupd,groupe,groupf,groupg,grouph]
+for g in allgroups:
+    gp = createteams(g)
+    table = playallmatches(gp)
+    t = table.sort_values(by='Pts',ascending=False)
     print(t)
-    
-def playmatch(team1,team2):
-    t1xpg = team1.attstrength * team2.defstrength * 1.18
-    t2xpg = team2.attstrength * team1.defstrength * 1.18
-    
-    t1probs = []
-    t2probs = []
-    scores = []
-    for i in range(0,25):
-        t1probs.append(ss.poisson.pmf(i,t1xpg))
-        t2probs.append(ss.poisson.pmf(i,t2xpg))
 
-    t1g = choice(range(25), 1, p=t1probs)[0]
-    t2g = choice(range(25), 1, p=t2probs)[0]
-    result = str(t1g) + "-" + str(t2g)
-    print('Result:',team1,result,team2)
-    
-    if t1g > t2g:
-        team1.update(3,t1g,t2g)
-        team2.update(0,t2g,t1g)
-    elif t2g  > t1g:
-        team2.update(3,t2g,t1g)
-        team1.update(0,t1g,t2g)
-    else:
-        team1.update(1,t1g,t2g)
-        team2.update(1,t2g,t1g)
-        
-groupa = [team(x) for x in[['Belgium',1.92,0.41],
-                    ['England',1.17,0.55],
-                    ['Tunisia',1.22,0.81],
-                    ['Panama',0.55,1.17]]]
-
-playallmatches(groupa)
+	
+## Simulate any match
+def simmatch(team1, team2,repeats):
+    t1wins = 0
+    t2wins = 0
+    draws = 0
+    for i in range(repeats):
+        t1,t2,d = playmatch(createteam(team1),createteam(team2))
+        t1wins += t1
+        t2wins += t2
+        draws += d
+    print(team1,":",t1wins)
+    print(team2,":",t2wins)
+    print("Draws:",draws)
